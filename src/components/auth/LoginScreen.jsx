6be9da7db0a79798a18189e56d74c2c2d9b3bc6c
alejandromunozc <alejandromunozc@gmail.com>
2021-05-12 22:16:32 -0500

    Configuración redux y preparación Firebase

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { login } from '../../actions/auth'
+import { useForm } from '../../hooks/useForm'
 
 export const LoginScreen = () => {
+
+  const dispatch = useDispatch();
+
+  const [formValues, handleInputChange] = useForm({
+    email: 'alejandromunozc@gmail.com',
+    password: '123456'
+  });
+
+  const {email, password} = formValues;
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dispatch(login(12345, 'Alejandro'));
+  }
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form action="">
-        <input type="text" placeholder="Email" name="email" autoComplete="off" className="auth__input" />
-        <input type="password" placeholder="password" name="password" className="auth__input" />
+      <form onSubmit={handleLogin}>
+        <input type="text" placeholder="Email" name="email" autoComplete="off" className="auth__input" value={email} onChange={handleInputChange} />
+        <input type="password" placeholder="password" name="password" className="auth__input" value={password} onChange={handleInputChange} />
         <button type="submit" className="btn btn-primary btn-block">
           Login
         </button>
